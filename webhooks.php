01ad26457f5e394cc2383d711bed97f1069af21a
sakmobile <sakmobile@users.noreply.github.com>
2020-06-24 13:23:22 +0700

    3

--- webhooks.php
+++ webhooks.php
@@ -1,51 +1,120 @@
-<?php // callback.php
+<?php 
 
 require "vendor/autoload.php";
+// include "admin/config.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'F4ptH38wFe7cYDr7B+0L+GxHxFDEixnLmeNIF5d+NYzh2ne0QgFNTNeceqyz95dwe+TW7DaVgb/qWr4RC9+M+xLFgCGx/BRF8UVaaHU1T1zIv/OqFUWytzW3mzZGbh0MiBqcLb0IJMqj30vQHWt7fgdB04t89/1O/w1cDnyilFU=';
+$access_token = "F4ptH38wFe7cYDr7B+0L+GxHxFDEixnLmeNIF5d+NYzh2ne0QgFNTNeceqyz95dwe+TW7DaVgb/qWr4RC9+M+xLFgCGx/BRF8UVaaHU1T1zIv/OqFUWytzW3mzZGbh0MiBqcLb0IJMqj30vQHWt7fgdB04t89/1O/w1cDnyilFU=";
 
-// Get POST body content
 $content = file_get_contents('php://input');
-// Parse JSON
 $events = json_decode($content, true);
-// Validate parsed JSON data
+
+
 if (!is_null($events['events'])) {
-	// Loop through each event
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
+	
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
+			
+			error_log($event['message']['text']);
+			$text = $event['message']['text'];
 			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
+			## เปิดสำหรับใช้่งาน mysql message
+			// $text = searchMessage($text ,$conn);
+			// $messages = setText($text);
+			$messages = setFlex();
+			sentToLine( $replyToken , $access_token  , $messages );
 		}
 	}
 }
-echo "OK";
+
+
+function setText( $text){
+	$messages = [
+		'type' => 'text',
+		'text' => $text
+	];
+	return $messages;
+}
+
+function setFlex(){
+	$message = '{
+		"type": "flex",
+		"altText": "Flex Message",
+		"contents": {
+		  "type": "bubble",
+		  "direction": "ltr",
+		  "header": {
+			"type": "box",
+			"layout": "vertical",
+			"contents": [
+			  {
+				"type": "text",
+				"text": "Header",
+				"align": "center"
+			  }
+			]
+		  },
+		  "hero": {
+			"type": "image",
+			"url": "https://developers.line.biz/assets/images/services/bot-designer-icon.png",
+			"size": "full",
+			"aspectRatio": "1.51:1",
+			"aspectMode": "fit"
+		  },
+		  "body": {
+			"type": "box",
+			"layout": "vertical",
+			"contents": [
+			  {
+				"type": "text",
+				"text": "Body",
+				"align": "center"
+			  }
+			]
+		  }
+		}
+	  }';
+	return $message;
+}
+
+function searchMessage($text , $conn){
+	$sql = "SELECT * FROM data where keyword='".$text."' ";
+	$result = $conn->query($sql);
+	
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$message = $row['intent'];
+		}
+	} else {
+		$message = "ไม่เข้าใจอ่ะ";
+	}
+	$conn->close();
+	return $message;
+}
+
+function sentToLine($replyToken , $access_token  , $messages ){
+	error_log("send");
+	$url = 'https://api.line.me/v2/bot/message/reply';
+	
+	$data = '{
+		"replyToken" : "'. $replyToken .'" ,
+		"messages" : ['. $messages .']
+	}';
+	$post = $data;
+
+	error_log($post);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	echo $result . "\r\n";
+	error_log($result);
+	error_log("send ok");
+}
+
+
