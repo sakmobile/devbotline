8941c2552802034e8027af22a982101728e2bfd8
sakmobile <sakmobile@users.noreply.github.com>
2020-06-25 08:50:15 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -88,60 +88,6 @@ function sentToLine($replyToken , $access_token  , $messages ){
 }
 
 function send_data( $data_podt){
-	function Curl($url, $post_data, &$http_status, &$header = null) {
-    
-
-		$ch=curl_init();
-		// user credencial
-		
-		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-	
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_URL, $url);
-	
-		// post_data
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-	
-		if (!is_null($header)) {
-			curl_setopt($ch, CURLOPT_HEADER, true);
-		}
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
-	
-		curl_setopt($ch, CURLOPT_VERBOSE, true);
-		
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	
-		$response = curl_exec($ch);
-		
-		  
-		$body = null;
-		// error
-		if (!$response) {
-			$body = curl_error($ch);
-			// HostNotFound, No route to Host, etc  Network related error
-			$http_status = -1;
-		   
-		} else {
-		   //parsing http status code
-			$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	
-			if (!is_null($header)) {
-				$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-	
-				$header = substr($response, 0, $header_size);
-				$body = substr($response, $header_size);
-			} else {
-				$body = $response;
-			}
-		}
-	
-		curl_close($ch);
-	
-		return $body;
-	}
 	
 	$url = "https://appealcovid19.xn--12cl1ck0bl6hdu9iyb9bp.com/appeal-web/api/appeal-api/personal-info/verify";
 	
@@ -170,6 +116,61 @@ if($res  == ""){
 		';
 	return $message;
 }
+}
+
+function Curl($url, $post_data, &$http_status, &$header = null) {
+    
+
+	$ch=curl_init();
+	// user credencial
+	
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, $url);
+
+	// post_data
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+
+	if (!is_null($header)) {
+		curl_setopt($ch, CURLOPT_HEADER, true);
+	}
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+
+	curl_setopt($ch, CURLOPT_VERBOSE, true);
+	
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+	$response = curl_exec($ch);
+	
+	  
+	$body = null;
+	// error
+	if (!$response) {
+		$body = curl_error($ch);
+		// HostNotFound, No route to Host, etc  Network related error
+		$http_status = -1;
+	   
+	} else {
+	   //parsing http status code
+		$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		if (!is_null($header)) {
+			$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+
+			$header = substr($response, 0, $header_size);
+			$body = substr($response, $header_size);
+		} else {
+			$body = $response;
+		}
+	}
+
+	curl_close($ch);
+
+	return $body;
 }
 
 
